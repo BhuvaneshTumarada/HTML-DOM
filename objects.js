c30e7c3c8a0d5a945153d5e5463f09ceee0deeab
Tumarada Bhuvanesh <tumaradabhuvanesh9@gmail.com>
2026-07-23 09:29:34 +0530

    Delete objects.js

--- objects.js
+++
@@ -1,7 +0,0 @@
-var person = { name:"sai", age:"19", city:"banglore"};
-console.log(typeof(person));
-console.log(person.name.length);
-person.color="fair";
-console.log(person);
-delete person.city;
-console.log(person);
