4f6b238f27699881c56fd52f8fbec3023cb8d157
Tumarada Bhuvanesh <tumaradabhuvanesh9@gmail.com>
2026-07-23 16:11:38 +0530

    Delete throw.js

--- throw.js
+++
@@ -1,9 +0,0 @@
-let age = 15;
-try {
-    if (age < 18) {
-        throw "You must be at least 18 years old.";
-    }
-    console.log("Access Granted");
-} catch (error) {
-    console.log(error);
-}
