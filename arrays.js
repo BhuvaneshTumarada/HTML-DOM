375fbce167bd05e9bb3aa8336e0dbcbfee003133
Tumarada Bhuvanesh <tumaradabhuvanesh9@gmail.com>
2026-07-23 09:35:52 +0530

    Delete arrays.js

--- arrays.js
+++
@@ -1,8 +0,0 @@
-let fruits=["Apple","Banana","Orange"];
-//fruits.push("Mango");
-//fruits.pop();
-//fruits.shift();
-//fruits.unshift("Grapes");
-//fruits.splice(1,1);
-console.log(fruits.slice(0,2));
-//console.log(fruits);
